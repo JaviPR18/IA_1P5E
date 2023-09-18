83b40d16fd6cb140146a3ae387db6962c82f649d
JaviPR18 <143574811+JaviPR18@users.noreply.github.com>
2023-09-18 00:08:41 -0600

    Add files via upload

---
+++ IA_P1_PRA20_ tuple.py
@@ -0,0 +1,6 @@
+
+colores = ['rojo', 'negro', 'gris', 'blanco', 'cafe', 'morado', 'azul', 'rosa', 'amarillo', 'naranja']
+
+colores_tupla = tuple(colores)
+
+print(type(colores_tupla))
---
+++ IA_P1_PRA21_ DIferente_que.py
@@ -0,0 +1,7 @@
+
+
+num1 = 18
+num2 = 21
+
+if num1 != num2:
+	print('Se ejecuta el if.')
